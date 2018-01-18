01b8d2341c0e5229fb2430d975e5731d2dc74a9c
Devon <k849996781@vip.qq.com>
2018-01-18 09:32:35 +0800

    no message

--- src/pages/more/more.ts
+++ src/pages/more/more.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the TabsPage page.
+ * Generated class for the MorePage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,19 +10,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-tabs',
-  templateUrl: 'tabs.html',
+  selector: 'page-more',
+  templateUrl: 'more.html',
 })
-export class TabsPage {
-
-  tabHome = 'HomePage';
-  tabMore = 'MorePage';
+export class MorePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TabsPage');
+    console.log('ionViewDidLoad MorePage');
   }
 
 }
--- src/pages/pages.constants.ts
+++ src/pages/pages.constants.ts
@@ -1,2 +1,3 @@
 //Constants for getting type references
 export const WO_PAGE = 'WoPage';
+export const MORE_PAGE = 'MorePage';
